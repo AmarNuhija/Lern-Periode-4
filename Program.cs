45921e06b0d126fb91d75b6b01f0eb339f043b66
AmarNuhija <142606791+AmarNuhija@users.noreply.github.com>
2024-03-15 16:46:24 +0100

    Add files via upload
    My dictionary and async awati C# Code for symptoms

--- Program.cs
+++ Program.cs
@@ -1,45 +1,78 @@
-﻿using BOTFORDISCORD2.config;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 
-namespace BOTFORDISCORD2
+namespace Arztneimittel
 {
     internal class Program
     {
-        public static DiscordIntents Intents { get; private set; }
-        public static string Token { get; private set; }
-        public static TokenType TokenType { get; private set; }
-        public static bool AutoReconnect { get; private set; }
-        private static DiscordClient Client { get; set; }
-        private static CommandsNextExtension Commands { get; set; }
-
         static async Task Main(string[] args)
         {
-            var jsonReader = new JSONreader();
-            await jsonReader.ReadJSON();
+            Dictionary<string, List<string>> Krankheit = new();
 
-            var discordconfig = new DiscordConfiguration()
+            Krankheit.Add("Kopfschmerzen", new List<string> { "Übelkeit", "Erbrechen", "Migräneanfälle" });
+            Krankheit.Add("Bauchschmerzen", new List<string> { "Blähungen", "Durchfall", "Gewichtsverlust" });
+            Krankheit.Add("Brustschmerzen", new List<string> { "Atemnot", "Angst", "Schweissausbrüche" });
+
+            string beschwerden;
+
+            do
             {
-                Intents = DiscordIntents.All,
-                Token = jsonReader.token,
-                TokenType = TokenType.Bot,
-                AutoReconnect = true
-            };
+                Console.WriteLine("Welche Beschwerden haben Sie? (Kopfschmerzen, Bauchschmerzen, Brustschmerzen oder 'exit' zum Beenden)");
+                beschwerden = Console.ReadLine();
 
-            Client = new DiscordClient(discordconfig);
+                if (Krankheit.ContainsKey(beschwerden))
+                {
+                    Console.WriteLine($"Die potentiellen Symptome von {beschwerden} sind:");
+                    foreach (var item in Krankheit[beschwerden])
+                    {
+                        Console.WriteLine($"\t{item}");
+                    }
 
-            Client.Ready += Client_Ready;
+                    Console.WriteLine("Wollen Sie die Krankheit heilen? (Ja/Nein)");
+                    string antwort = Console.ReadLine();
 
+                    antwort = antwort.ToLower();
 
-            await Client.ConnectAsync();
-            await Task.Delay(-1);           //Das hier mit -1 bringt uns das solange ich dieses Programm starte, das es die ganze Zeit auf Discord läuft.
-             }
+                    if (antwort == "ja")
+                    {
+                        Console.WriteLine("Nehmen Sie eine Tablette");
+                        await SummonTabletteLocally(); // Hier wird die Funktion aufgerufen, um die Tablette anzuzeigen
+                    }
+                    else if (antwort == "nein")
+                    {
+                        Console.WriteLine("Dann selber schuld");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie Ja oder Nein ein");
+                    }
 
-            private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+                    break; // Beende die Schleife nachdem die Antwort auf die Frage erhalten wurde
+                }
+                else if (beschwerden.ToLower() == "exit")
+                {
+                    Console.WriteLine("Das Programm wurde beendet.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe.");
+                }
 
-            {
-                return Task.CompletedTask;
-            }
+            } while (true);
+
+        }
+
+        static async Task SummonTabletteLocally()
+        {
+            Console.WriteLine($"Tablette wird erstellt...");
+            string filePath = "C:\\Users\\AMAR\\OneDrive - Alte Kantonsschule Aarau\\Desktop\\Tablette.txt";
+
+            string tabletteText = await File.ReadAllTextAsync(filePath);
+            Console.WriteLine($"Erstellte Tablette:\n{tabletteText}");
+        }
     }
 }
