e6cd6ade4c617944b104f0f014022e629572c4c7
AmarNuhija <142606791+AmarNuhija@users.noreply.github.com>
2024-03-22 16:42:21 +0100

    Add files via upload
    SnakeGameForBeginnr

--- Program.cs
+++ Program.cs
@@ -1,78 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Net.Http.Headers;
 
-namespace Arztneimittel
+namespace SnakeGameReaL
 {
-    internal class Program
+     class Program
     {
-        static async Task Main(string[] args)
+        int Hight = 10;
+        int Widht = 30;
+
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int fruitX;
+        int fruitY;
+
+        int parts = 3;
+
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'W';
+
+        Random rnd = new Random();
+
+        Program()
         {
-            Dictionary<string, List<string>> Krankheit = new();
-
-            Krankheit.Add("Kopfschmerzen", new List<string> { "Übelkeit", "Erbrechen", "Migräneanfälle" });
-            Krankheit.Add("Bauchschmerzen", new List<string> { "Blähungen", "Durchfall", "Gewichtsverlust" });
-            Krankheit.Add("Brustschmerzen", new List<string> { "Atemnot", "Angst", "Schweissausbrüche" });
-
-            string beschwerden;
-
-            do
-            {
-                Console.WriteLine("Welche Beschwerden haben Sie? (Kopfschmerzen, Bauchschmerzen, Brustschmerzen oder 'exit' zum Beenden)");
-                beschwerden = Console.ReadLine();
-
-                if (Krankheit.ContainsKey(beschwerden))
-                {
-                    Console.WriteLine($"Die potentiellen Symptome von {beschwerden} sind:");
-                    foreach (var item in Krankheit[beschwerden])
-                    {
-                        Console.WriteLine($"\t{item}");
-                    }
-
-                    Console.WriteLine("Wollen Sie die Krankheit heilen? (Ja/Nein)");
-                    string antwort = Console.ReadLine();
-
-                    antwort = antwort.ToLower();
-
-                    if (antwort == "ja")
-                    {
-                        Console.WriteLine("Nehmen Sie eine Tablette");
-                        await SummonTabletteLocally(); // Hier wird die Funktion aufgerufen, um die Tablette anzuzeigen
-                    }
-                    else if (antwort == "nein")
-                    {
-                        Console.WriteLine("Dann selber schuld");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie Ja oder Nein ein");
-                    }
-
-                    break; // Beende die Schleife nachdem die Antwort auf die Frage erhalten wurde
-                }
-                else if (beschwerden.ToLower() == "exit")
-                {
-                    Console.WriteLine("Das Programm wurde beendet.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Ungültige Eingabe.");
-                }
-
-            } while (true);
-
+            X[0] = 5;
+            Y[1] = 5;
+            Console.CursorVisible = false;
+             fruitX = rnd.Next(2, (Widht - 2));
+             fruitY = rnd.Next(2,(Hight - 2));
         }
-
-        static async Task SummonTabletteLocally()
+        public void WriteBoard()
         {
-            Console.WriteLine($"Tablette wird erstellt...");
-            string filePath = "C:\\Users\\AMAR\\OneDrive - Alte Kantonsschule Aarau\\Desktop\\Tablette.txt";
+            Console.Clear();
+            for (int i = 1; i <= (Widht + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Widht + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Hight+2));
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (Hight + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("|");
+            }
+            for (int i = 1; i <= (Hight + 1); i++)
+            {
+                Console.SetCursorPosition((Widht+2), i);
+                Console.Write("|");
+            }
+ 
+        }
+        public void Input()
+        {
+            if(Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+        }
+        public void WritePoint(int x,int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("I");
+        }
+        public void Logic()
+        {
+            if (X[0] == fruitX)
+            {
+                if (Y[0] == fruitY)
+                {
+                    parts++;
+                        fruitX = rnd.Next(2, (Widht - 2));
+                        fruitY = rnd.Next(2,(Hight - 2));
+                }
+            }
+            for (int i = parts; i > 1; i--)
+            {
+                X[i-1] = X[i-2];
+                Y[i-1] = Y[i-2];
+            }
+            switch (key)
+            {
+                case 'w':
+                    Y[0]--;
+                        break;
+                case 's':
+                    Y[0]++;
+                    break;
+                case 'd':
+                    X[0]++;
+                    break;
+                case 'a':
+                    X[0]--;
+                    break;
+            }
+            for (int i = 0; i <= (parts - 1); i++)
+            {
+                WritePoint(X[i], Y[i]);
+                WritePoint(fruitX, fruitY);
+            }
+            Thread.Sleep(100);
+        }
+        static void Main(string[] args)
+        {
 
-            string tabletteText = await File.ReadAllTextAsync(filePath);
-            Console.WriteLine($"Erstellte Tablette:\n{tabletteText}");
+        Program snake = new Program();
+            while (true)
+            {
+                snake.WriteBoard();
+                snake.Input();
+                snake.Logic();
+            }
+                Console.ReadKey();
+            
+
         }
     }
 }
